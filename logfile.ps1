bd739f9291cb39ce8085f67e656fb61939180736
mikefoley <mike@yelof.com>
2015-05-27 15:10:08 -0400

    Lots-o-changes
    Fixed missing quote, proper network naming, etc.. Whole host of dumb
    mistakes.

---
+++ logfile.ps1
@@ -0,0 +1,4 @@
+﻿#logfile
+#Write-Host "Output a file onto the desktop showing that everything has completed."
+#`logdate = get-date -formate "dddd-mmm-yy HH.sstt"
+#`logfile = 'c:\Domain-Controller-Script-Completed at '+`$logdate+".txt"
